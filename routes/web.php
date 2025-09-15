2b3298f3a5af437ea03679882bbe9eb28cf745f4
ThangLoiNguyen <thangloinguyen1209@gmail.com>
2025-09-15 11:20:07 +0700

    Initial commit

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EmailController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [EmailController::class, 'index'])->name('emails.index');
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('index');
 Route::resource('emails', EmailController::class);
 Route::post('/emails/{id}/restore', [EmailController::class, 'restore'])->name('emails.restore');
 
